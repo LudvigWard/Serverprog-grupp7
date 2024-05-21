9b7c385a420316f369dddeebc073a2a2e2414566
Ludvig Ward <ludvig@upward.se>
2024-05-21 14:47:55 +0200

    Initial commit

---
+++ src/main/java/se/yrgo/ServerprogGrupp7Application.java
@@ -0,0 +1,13 @@
+package se.yrgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerprogGrupp7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerprogGrupp7Application.class, args);
+	}
+
+}
