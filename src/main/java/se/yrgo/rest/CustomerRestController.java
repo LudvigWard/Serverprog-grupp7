8c7af8e27a6beb5f12184a5dd1bfdf225fc5e9ea
Älva <alvlac0326@skola.goteborg.se>
2024-05-23 18:39:35 +0200

    It now works to input data to the customers table

--- src/main/java/se/yrgo/rest/CustomerRestController.java
+++ src/main/java/se/yrgo/rest/CustomerRestController.java
@@ -3,6 +3,7 @@ package se.yrgo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.*;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
 import se.yrgo.data.CustomerRepo;
@@ -18,5 +19,11 @@ public class CustomerRestController {
     public CustomerList allCustomers() {
         List<Customer> all = data.findAll();
         return new CustomerList(all);
+    }
+
+    @RequestMapping(value="/customers", method=RequestMethod.POST)
+    public ResponseEntity createNewCustomer(@RequestBody Customer customer){
+        data.save(customer);
+        return new ResponseEntity<Customer>(customer, HttpStatus.CREATED);
     }
 }
