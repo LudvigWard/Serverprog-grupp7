a9975653332537820457b22aaf34fda6210f853c
Älva <alvlac0326@skola.goteborg.se>
2024-05-23 19:38:40 +0200

    implemented fetch data in MainrestController for each list

--- src/main/java/se/yrgo/rest/MainRestController.java
+++ src/main/java/se/yrgo/rest/MainRestController.java
@@ -6,24 +6,66 @@ import org.springframework.beans.factory.annotation.*;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
-import se.yrgo.data.CustomerRepo;
-import se.yrgo.domain.Customer;
+import se.yrgo.data.*;
+import se.yrgo.domain.*;
 
 @RestController
-public class CustomerRestController {
-    
+public class MainRestController {
+
     @Autowired
-    private CustomerRepo data;
+    private CustomerRepo customerData;
+    @Autowired
+    private CustomerOrderRepo customerOrderData;
+    @Autowired
+    private OrderProductRepo orderProductData;
+    @Autowired
+    private ProductRepo productData;
 
     @RequestMapping("/customers")
     public CustomerList allCustomers() {
-        List<Customer> all = data.findAll();
+        List<Customer> all = customerData.findAll();
         return new CustomerList(all);
     }
 
-    @RequestMapping(value="/customers", method=RequestMethod.POST)
-    public ResponseEntity createNewCustomer(@RequestBody Customer customer){
-        data.save(customer);
-        return new ResponseEntity<Customer>(customer, HttpStatus.CREATED);
+    @RequestMapping(value = "/customers", method = RequestMethod.POST)
+    public ResponseEntity<Customer> createNewCustomer(@RequestBody Customer customer) {
+        customerData.save(customer);
+        return new ResponseEntity<>(customer, HttpStatus.CREATED);
+    }
+
+    @RequestMapping("/customerOrders")
+    public CustOrderList allCustOrders() {
+        List<CustomerOrder> all = customerOrderData.findAll();
+        return new CustOrderList(all);
+    }
+
+    @RequestMapping(value = "/customerOrders", method = RequestMethod.POST)
+    public ResponseEntity<CustomerOrder> createNewCustOrder(@RequestBody CustomerOrder customerOrder) {
+        customerOrderData.save(customerOrder);
+        return new ResponseEntity<>(customerOrder, HttpStatus.CREATED);
+    }
+
+    @RequestMapping("/orderProducts")
+    public OrderProductList allOrderProducts() {
+        List<OrderProduct> all = orderProductData.findAll();
+        return new OrderProductList(all);
+    }
+
+    @RequestMapping(value = "/orderProducts", method = RequestMethod.POST)
+    public ResponseEntity<OrderProduct> createNewOrderProduct(@RequestBody OrderProduct orderProduct) {
+        orderProductData.save(orderProduct);
+        return new ResponseEntity<>(orderProduct, HttpStatus.CREATED);
+    }
+
+    @RequestMapping("/products")
+    public ProductList allProducts() {
+        List<Product> all = productData.findAll();
+        return new ProductList(all);
+    }
+
+    @RequestMapping(value = "/products", method = RequestMethod.POST)
+    public ResponseEntity<Product> createNewProduct(@RequestBody Product product) {
+        productData.save(product);
+        return new ResponseEntity<>(product, HttpStatus.CREATED);
     }
 }
