1188157b47da3a3b5aa2b9ff56a5e994e3aeeb6f
Älva <alvlac0326@skola.goteborg.se>
2024-05-27 14:09:10 +0200

    correcting bridge table post methods and also creating single or multiple insertion post methods

--- src/main/java/se/yrgo/rest/MainRestController.java
+++ src/main/java/se/yrgo/rest/MainRestController.java
@@ -1,10 +1,12 @@
 package se.yrgo.rest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import se.yrgo.data.*;
 import se.yrgo.domain.*;
@@ -32,10 +34,18 @@ public class MainRestController {
         return new CustomerList(all);
     }
 
-    @PostMapping("/customers")
-    public ResponseEntity<Customer> createNewCustomer(@RequestBody Customer customer) {
-        customerData.save(customer);
-        return new ResponseEntity<>(customer, HttpStatus.CREATED);
+    @PostMapping("/addCustomers")
+    public ResponseEntity<Object> createNewCustomers(@RequestBody List<Customer> customers) {
+        if (customers.size() == 1) {
+            // If only one customer is provided, save it and return
+            Customer singleCustomer = customers.get(0);
+            customerData.save(singleCustomer);
+            return new ResponseEntity<>(singleCustomer, HttpStatus.CREATED);
+        } else {
+            // If multiple customers are provided, save them and return
+            List<Customer> savedCustomers = customerData.saveAll(customers);
+            return new ResponseEntity<>(savedCustomers, HttpStatus.CREATED);
+        }
     }
 
     @RequestMapping("/customerOrders")
@@ -44,10 +54,28 @@ public class MainRestController {
         return new CustOrderList(all);
     }
 
-    @PostMapping("/customerOrders")
+    @PostMapping("/addCustomerOrder")
     public ResponseEntity<CustomerOrder> createNewCustOrder(@RequestBody CustomerOrder customerOrder) {
+        Customer customer = customerData.findById(customerOrder.getCustomer().getCustomerId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Customer not found"));
+
+        customerOrder.setCustomer(customer);
         customerOrderData.save(customerOrder);
         return new ResponseEntity<>(customerOrder, HttpStatus.CREATED);
+    }
+
+    // Endpoint for adding multiple customer orders
+    @PostMapping("/addMultipleCustomerOrders")
+    public ResponseEntity<List<CustomerOrder>> createNewCustOrders(@RequestBody List<CustomerOrder> customerOrders) {
+        List<CustomerOrder> savedOrders = new ArrayList<>();
+        for (CustomerOrder order : customerOrders) {
+            Customer customer = customerData.findById(order.getCustomer().getCustomerId())
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Customer not found"));
+            order.setCustomer(customer);
+            savedOrders.add(order);
+        }
+        savedOrders = customerOrderData.saveAll(savedOrders);
+        return new ResponseEntity<>(savedOrders, HttpStatus.CREATED);
     }
 
     @RequestMapping("/orderProducts")
@@ -56,10 +84,26 @@ public class MainRestController {
         return new OrderProductList(all);
     }
 
-    @PostMapping("/orderProducts")
+    @PostMapping("/addOrderProduct")
     public ResponseEntity<OrderProduct> createNewOrderProduct(@RequestBody OrderProduct orderProduct) {
+        customerOrderData.save(orderProduct.getCustomerOrder());
+        productData.save(orderProduct.getProduct());
         orderProductData.save(orderProduct);
         return new ResponseEntity<>(orderProduct, HttpStatus.CREATED);
+    }
+
+    // Endpoint for adding multiple order products in a batch
+    @PostMapping("/addMultipleOrderProducts")
+    public ResponseEntity<List<OrderProduct>> createNewOrderProductsBatch(
+            @RequestBody List<OrderProduct> orderProducts) {
+        List<OrderProduct> savedProducts = new ArrayList<>();
+        for (OrderProduct product : orderProducts) {
+            customerOrderData.save(product.getCustomerOrder());
+            productData.save(product.getProduct());
+            savedProducts.add(product);
+        }
+        savedProducts = orderProductData.saveAll(savedProducts);
+        return new ResponseEntity<>(savedProducts, HttpStatus.CREATED);
     }
 
     @RequestMapping("/products")
@@ -68,9 +112,17 @@ public class MainRestController {
         return new ProductList(all);
     }
 
-    @PostMapping("/products")
+    @PostMapping("/product")
     public ResponseEntity<Product> createNewProduct(@RequestBody Product product) {
         productData.save(product);
         return new ResponseEntity<>(product, HttpStatus.CREATED);
     }
+
+    // Endpoint for adding multiple products
+    @PostMapping("/addMultipleProducts")
+    public ResponseEntity<List<Product>> createNewProducts(@RequestBody List<Product> products) {
+        List<Product> savedProducts = productData.saveAll(products);
+        return new ResponseEntity<>(savedProducts, HttpStatus.CREATED);
+    }
+
 }
